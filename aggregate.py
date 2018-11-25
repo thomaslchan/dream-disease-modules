42152dcd4d887f01c0831c5b41773214dfafe094
Edwin Jain <Edwin.Jain@tufts.edu>
2018-11-25 04:32:11 -0500

    Copied over weighted DSD code, cleaned read_data, and created a basic EDA table

--- aggregate.py
+++ aggregate.py
@@ -1,24 +1,77 @@
-import sys
+from __future__ import division
+import sys, os, operator, time
+
 import networkx as nx
-import os
+import numpy as np
+from numpy.linalg import inv
+from scipy.spatial.distance import pdist, squareform
+from prettytable import PrettyTable
+
+DATA = 'projdata/1_networks/original'
+
 
 def read_data(path):
-	graphs = []
-	i = 1
-	for filename in os.listdir(path):
-		print (i)
-		i += 1
-		edges = []
-		with open(os.path.join(path, filename)) as file:
-                        for line in file.readlines():
-			        edges.append(line.strip('\r\n').split('\t'))
-		G = nx.MultiGraph()
-		G.add_weighted_edges_from(edges)
-		graphs.append(G)
-	return graphs
+    graphs = []
+    i = 0
+    for filename in sorted(os.listdir(path)):
+        print(filename)
+        start = time.time()
+        i += 1
+        g_type = nx.OrderedDiGraph if i == 3 else nx.OrderedGraph
+        G = nx.read_weighted_edgelist(os.path.join(path, filename),
+                                    create_using=g_type)
+        graphs.append(G)
+        end = time.time()
+        print("Reading in network " + str(i) + " took " 
+                + str(int(end-start)) + " seconds.\n\n")
+    return graphs
 
-def main(argv):
-	graphs = read_data(argv[1])
+
+def build_transition_matrix(adjacency_graph):
+    degs = adjacency_graph.sum(axis=1)
+
+    transition = adjacency_graph / degs[:,None]
+    transition[degs==0,:] = 0
+    transition[degs==0,degs==0] = 1
+    return transition
+
+
+def calc_hescotts(transition, iters, v=True, n=None):
+    # params v and n are not used, left for compatibility with original dsdcore script
+    nRw = iters
+    p = transition
+    n = p.shape[0]
+    c = np.eye(n)
+    c0 = np.eye(n)
+    for i in xrange(nRw):
+        c = np.dot(c, p) + c0
+    return c
+
+
+def calc_dsd(transition):
+    return squareform(pdist(transition,metric='cityblock'))              
+
+
+def add_self_edges(adjacency_graph, base_weight=1):    
+    n = np.size(adjacency_graph[0])
+    ident = np.identity(n)*base_weight
+    return np.add(adjacency_graph,ident)
+
+
+def show_eda_table(graphs):
+    cols = ['Nodes', 'Edges', 'Size of Largest Component', 
+            'Mean Edge Weight', 'Std. Dev. Edge Weight']
+    t = PrettyTable(cols)
+    for G in graphs:
+        info = []
+        info += [nx.number_of_nodes(G), nx.number_of_edges(G)]
+        info.append(len(max(nx.connected_components(G), key=len)))
+        edge_weights = [d['weight'] for (n1, n2, d) in G.edges(data=True)]
+        info += [np.mean(edge_weights), np.std(edge_weights)]
+        t.add_row(info)
+    print(t)
+
 
 if __name__ == '__main__':
-        main(sys.argv)
+    graphs = read_data(DATA)
+    show_eda_table(graphs)
